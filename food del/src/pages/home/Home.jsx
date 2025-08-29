23b8b1aba59ddf12d7865907dd4e2587cd7e44d0
Devang Swami <devangswami26@gmail.com>
2025-08-29 20:24:38 +0530

    Add files via upload

---
+++ food del/src/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Header/header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import Appdownload from '../../components/AppDownload/Appdownload'
+
+
+
+const Home = () => {
+
+    const [category,setCategory] =useState("All")
+
+
+  return (
+    <div>
+        <Header/>
+        <ExploreMenu category={category} setCategory={setCategory} />
+        <FoodDisplay category={category}/>
+        <Appdownload/>
+    </div>
+  )
+}
+
+export default Home
+
