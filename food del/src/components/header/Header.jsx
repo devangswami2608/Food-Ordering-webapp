2673549955ba6b9686b39f86c7173a3f3816dcd7
Devang Swami <devangswami26@gmail.com>
2025-08-29 20:19:48 +0530

    Add files via upload

---
+++ food del/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h1>Order Your Food here       </h1>
+            <p> Great Food at he great places choose food from a diverse place. Your craving will be satisfied and enhance your dining experience.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
